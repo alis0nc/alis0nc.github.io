8e9b9b765c7650e9b67d0bc9dfc5aa526e811ea9
Alison C <alisonc@alisonc.net>
2017-11-30 17:52:39 -0500

    remove js directory

--- js/emojione-convert.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    var inputs = document.getElementsByClassName("emojione");
-    var i;
-    for (i = 0; i < inputs.length; i++) {
-    	inputs[i].innerHTML = emojione.toImage(inputs[i].textContent);
-    }
-  }
-
